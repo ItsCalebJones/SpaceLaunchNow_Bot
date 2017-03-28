a8b34be0429eb38b02d5688c49d333cba3f2612e
Caleb Jones <ca.jones9119@gmail.com>
2017-03-28 00:11:33 -0400

    Add Twitter client, start work on analyzing data.

--- models.py
+++ models.py
@@ -1,1 +1,70 @@
-__author__ = 'Caleb'
+from tinydb import TinyDB, Query
+
+db = TinyDB('H:\GitHub\Space-Launch-Now-Server\db.json')
+
+
+class Launch:
+    def __init__(self, launch):
+        self.launch_id = launch["id"]
+        self.launch_name = launch["name"]
+        self.status = launch["status"]
+        self.net_stamp = launch["netstamp"]
+        self.data = launch
+        self.notified = False
+
+        if len(launch["location"]["pads"]) > 0:
+            self.location = launch["location"]["pads"][0]["name"]
+
+        if len(launch["missions"]) > 0:
+            self.missions = launch["missions"]
+
+        self.twitter_table = db.table('twitter')
+
+        response = self.twitter_table.search(Query().launch == self.launch_id)
+        if len(response) > 0:
+            self.last_twitter_post = response[len(response) - 1]['last_twitter_update']
+        else:
+            self.last_twitter_post = None
+
+        self.launch_table = db.table('launch')
+
+        launch_cache = self.launch_table.search(Query().launch == self.launch_id)
+        if launch_cache:
+            self.wasNotifiedTwentyFourHour = launch_cache[0]['isNotified24']
+            self.wasNotifiedOneHour = launch_cache[0]['isNotifiedOne']
+            self.wasNotifiedTenMinutes = launch_cache[0]['isNotifiedTen']
+            self.notified = launch_cache[0]['notified']
+
+    def reset_notifiers(self):
+        self.wasNotifiedTwentyFourHour = False
+        self.wasNotifiedOneHour = False
+        self.wasNotifiedTenMinutes = False
+        self.update_record()
+
+    def is_notified_24(self, boolean):
+        self.wasNotifiedTwentyFourHour = boolean
+        self.update_record()
+
+    def is_notified_one_hour(self, boolean):
+        self.wasNotifiedOneHour = boolean
+        self.update_record()
+
+    def is_notified_ten_minutes(self, boolean):
+        self.wasNotifiedTenMinutes = boolean
+        self.update_record()
+
+    def update_record(self):
+        if self.notified is False:
+            self.notified = True
+            self.launch_table.insert({'launch': self.launch_id,
+                                      'isNotified24': self.wasNotifiedTwentyFourHour,
+                                      'isNotifiedOne': self.wasNotifiedOneHour,
+                                      'isNotifiedTen': self.wasNotifiedTenMinutes, 'notified': self.notified})
+        else:
+            self.launch_table.update({'isNotified24': self.wasNotifiedTwentyFourHour,
+                                      'isNotifiedOne': self.wasNotifiedOneHour,
+                                      'isNotifiedTen': self.wasNotifiedTenMinutes, 'notified': self.notified},
+                                     Query().launch == self.launch_id)
+
+
+
--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import datetime
 
 from config import keys
 from twitter import *
+from models import Launch
 from onesignalsdk import OneSignalSdk
 from launchlibrarysdk import LaunchLibrarySDK
 from tinydb import TinyDB, Query
@@ -25,6 +26,7 @@ class SpaceLaunchNowNotificationServer:
     def __init__(self):
         self.onesignal = OneSignalSdk(AUTH_TOKEN_HERE, APP_ID)
         self.launchLibrary = LaunchLibrarySDK()
+        self.diff = None
         response = self.onesignal.get_app(APP_ID)
         assert response.status_code == 200
         self.app = response.json()
@@ -34,67 +36,86 @@ class SpaceLaunchNowNotificationServer:
         self.twitter = Twitter(
             auth=OAuth(keys['TOKEN_KEY'], keys['TOKEN_SECRET'], keys['CONSUMER_KEY'], keys['CONSUMER_SECRET'])
         )
+        self.twitter_table = db.table('twitter')
+        self.launch_table = db.table('launch')
 
     def send_to_twitter(self, message, launch):
         # self.twitter.statuses.update(status=message)
         self.twitter.direct_messages.new(
             user="ItsCalebJones",
             text=message)
-        if launch.last_twitter_update is None:
-            db.insert({'launch': launch.launch_id, 'last_twitter_update': time.time()})
+        if launch.last_twitter_post is None:
+            self.twitter_table.insert({'launch': launch.launch_id, 'last_twitter_update': time.time()})
         else:
-            db.update({'last_twitter_update': time.time()}, Query().launch == launch.launch_id)
+            self.twitter_table.update({'last_twitter_update': time.time()}, Query().launch == launch.launch_id)
 
     def check_next_launch(self):
         response = self.launchLibrary.get_next_launches()
         launch_data = response.json()
         for launches in launch_data["launches"]:
             launch = Launch(launches)
-            if launch.netstamp > 0:
+            if launch.net_stamp > 0:
                 current_time = datetime.datetime.utcnow()
-                launch_time = datetime.datetime.utcfromtimestamp(int(launch.netstamp))
+                launch_time = datetime.datetime.utcfromtimestamp(int(launch.net_stamp))
                 if current_time <= launch_time:
-                    diff = int((launch_time - current_time).total_seconds())
-                    print diff
-                    if launch.last_twitter_update is not None:
-                        time_since_last_twitter_update = (current_time - datetime.datetime.utcfromtimestamp(
-                            int(launch.last_twitter_update))).total_seconds()
-                        print 'Seconds since last update on Twitter %d' % time_since_last_twitter_update
-                        if diff < 86400 & diff > 3600:
-                            if time_since_last_twitter_update > 43200:
-                                self.send_to_twitter('%s launching from %s in %s' %
-                                                     (launch.launchName, launch.location, self.seconds_to_time(diff)),
-                                                     launch)
-                        if diff < 3600 & diff > 3600:
-                            if time_since_last_twitter_update > 3600:
-                                self.send_to_twitter('%s launching from %s in %s' %
-                                                     (launch.launchName, launch.location, self.seconds_to_time(diff)),
-                                                     launch)
-                    else:
-                        print 'Launch has not been posted to Twitter.'
-                        self.send_to_twitter('%s launching from %s in %s' % (
-                            launch.launchName, launch.location, self.seconds_to_time(diff)), launch)
+                    self.diff = int((launch_time - current_time).total_seconds())
+                    self.check_twitter(self.diff, launch)
+                    self.check_launch_window(self.diff, launch)
+
+    def check_twitter(self, diff, launch):
+        if launch.last_twitter_post is not None:
+            time_since_last_twitter_update = (datetime.datetime.utcnow() - datetime.datetime.utcfromtimestamp(
+                int(launch.last_twitter_post))).total_seconds()
+            print 'Seconds since last update on Twitter %d' % time_since_last_twitter_update
+            if diff < 86400:
+                if diff > 3600:
+                    if time_since_last_twitter_update > 43200:
+                        self.send_to_twitter('%s launching from %s in %s' %
+                                             (launch.launch_name, launch.location, self.seconds_to_time(diff)),
+                                             launch)
+                if diff < 3600:
+                    if time_since_last_twitter_update > 3600:
+                        self.send_to_twitter('%s launching from %s in %s' %
+                                             (launch.launch_name, launch.location, self.seconds_to_time(diff)),
+                                             launch)
+        else:
+            self.log('Launch has not been posted to Twitter.')
+            self.send_to_twitter('%s launching from %s in %s' % (launch.launch_name, launch.location,
+                                                                 self.seconds_to_time(diff)), launch)
+
+    def check_launch_window(self, diff, launch):
+        # If launch is within 24 hours...
+        if diff <= 86400:
+            self.send_notification(launch)
+            launch.is_notified_24(True)
+        elif diff <= 3600:
+            self.send_notification(launch)
+            launch.is_notified_one_hour(True)
+        elif diff <= 600:
+            launch.is_notified_ten_minutes(True)
 
     def send_notification(self, launch):
         self.onesignal.user_auth_key = self.app_auth_key
         self.onesignal.app_id = APP_ID
+        self.log('Creating notification for %s' % launch.launch_name)
 
         # Create a notification
         contents = '%s launching from %s' % (launch.launchName, launch.location)
         kwargs = dict(
-            android_group="thisGroup",
+            content_available=True,
             included_segments=['Debug'],
             isAndroid=True,
-            small_icon='ic_rocket_white',
-            large_icon='ic_human_explore'
+            data={"silent": True}
         )
         url = 'https://launchlibrary.net'
         heading = 'Space Launch Now'
         response = self.onesignal.create_notification(contents, heading, url, **kwargs)
         assert response.status_code == 200
+
         notification_data = response.json()
         notification_id = notification_data['id']
         assert notification_data['id'] and notification_data['recipients']
+
         # Get the notification
         response = self.onesignal.get_notification(APP_ID, notification_id, self.app_auth_key)
         notification_data = response.json()
@@ -106,11 +127,15 @@ class SpaceLaunchNowNotificationServer:
 
     def run(self):
         """The daemon's main loop for doing work"""
-        self.log('Space Launch Now daemon is now running...')
+        self.log('Space Launch Now daemon is now running.')
         while True:
             self.check_next_launch()
-            self.log('Sleeping for %d seconds...' % DAEMON_SLEEP)
-            time.sleep(DAEMON_SLEEP)
+            if self.diff > 600:
+                self.log('Next launch in %i, sleeping for %d seconds.' % (self.diff, DAEMON_SLEEP))
+                time.sleep(DAEMON_SLEEP)
+            else:
+                self.log('Sleeping for %d seconds.' % self.diff)
+                time.sleep(self.diff)
 
     def seconds_to_time(self, seconds):
         seconds_in_day = 86400
@@ -126,26 +151,6 @@ class SpaceLaunchNowNotificationServer:
         minutes = seconds // seconds_in_minute
         seconds -= minutes * seconds_in_minute
         return "{0:.0f} days, {1:.0f} hours, {2:.0f} minutes.".format(days, hours, minutes, seconds)
-
-
-class Launch:
-    def __init__(self, launch):
-        self.launch_id = launch["id"]
-        self.launchName = launch["name"]
-        self.status = launch["status"]
-        self.netstamp = launch["netstamp"]
-        if len(launch["location"]["pads"]) > 0:
-            self.location = launch["location"]["pads"][0]["name"]
-        if len(launch["missions"]) > 0:
-            self.missions = launch["missions"]
-        response = db.search(Query().launch == self.launch_id)
-        if len(response) > 0:
-            self.last_twitter_update = response[len(response) - 1]['last_twitter_update']
-        else:
-            self.last_twitter_update = None
-
-    def record_twitter_update(self, time):
-        pass
 
 
 if __name__ == '__main__':
